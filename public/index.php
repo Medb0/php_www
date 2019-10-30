2547ae69f2a49e804af05f4e974f681c8a011d9a
Medb0 <bxcv1230@gmail.com>
2019-10-30 12:24:59 +0900

    테이블 함수 변환

--- public/index.php
+++ public/index.php
@@ -12,14 +12,39 @@ $query = "SHOW TABLES";
 $result = $db->queryExecute($query);
 $count = mysqli_num_rows($result);
 $content = "";
+
 for ($i=0; $i < $count ; $i++) {
   $row = mysqli_fetch_object($result);
-  $content .= "<tr>";
-  $content .= "<td>$i</td>";
-  $content .= "<td>".$row->Tables_in_php."</td>";
-  $content .= "</tr>";
+  $rows [] = $row;
 }
 
+$content = table($rows);
 $body = file_get_contents("../Resource/table.html");
 $body = str_replace("{{content}}", $content, $body);
 echo $body;
+
+function table($rows)
+{
+  $body = "<table class=\"table\">";
+
+
+  $body .="<thead>";
+  $body .="<tr>
+  <th>No.</th>
+  <th>Name</th>
+  </tr>";
+  $body .= "</thead>";
+  $body .= "<tbody>";
+
+  for($i=0;$i<count($rows);$i++){
+    $body .= "<tr>";
+    $body .= "<td>$i</td>";
+    $body .= "<td>".$rows[$i]->Tables_in_php."</td>";
+    $body .= "</tr>";
+  }
+
+  $body .= "</tbody>";
+  $body .=  "</table>";
+
+  return $body;
+}
