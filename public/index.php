af9a7be4bfcb5b75afcc46bf1006a4a8c1d1dad2
Medb0 <bxcv1230@gmail.com>
2019-10-30 11:20:16 +0900

    개노잼 복붙

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
-echo "dealim";
+$config = include '../dbconf.php';
+print_r($config);
+
+require '../Module/Database/Database.php';
+require '../Module/Database/Table.php';
+
+$db = new Database($config);
+echo "<br>";
+$query = "SHOW TABLES";
+$result = $db->queryExecute($query);
+
+$count = mysqli_num_rows($result);
+for ($i=0; $i < $count ; $i++) {
+  $row = mysqli_fetch_object($result);
+  echo $row->Tables_in_php."<br>";
+}
